2ebd95d56e66e2da01f31257615c17c1b621456e
glunya <glunya@mail.ua>
2020-04-22 09:11:42 +0200

    Added range slider to the simulation plot

--- dash_app/utils/plotting.py
+++ dash_app/utils/plotting.py
@@ -15,18 +15,16 @@ def plot_dataframe(dataframe: pd.DataFrame,max_y_axis: int = None) -> Dict[str, 
     else:
         yaxis = {"range": (0, max_y_axis), "autorange": False}
 
-    return {
-        "data": [
-            {
-                "x": dataframe.index,
-                "y": dataframe[col].astype(int),
-                "name": col,
-                "mode": "lines+markers",
-            }
-            for col in dataframe.columns
-        ],
-        "layout": {
-            "yaxis": yaxis,
-            "legend": {"orientation": "h"},
-        },
-    }
+    return {'data': [
+        {
+            "x": dataframe.index,
+            "y": dataframe[col].astype(int),
+            "name": col,
+            "mode": "lines+markers",
+        }
+        for col in dataframe.columns
+    ], 'layout': {
+        "yaxis": yaxis,
+        "legend": {"orientation": "h"},
+        "xaxis": {"rangeslider": {}}
+    }}
