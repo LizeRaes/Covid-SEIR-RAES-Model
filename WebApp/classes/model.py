abcd19ef8d534ad361ba3450f5d0720df840410b
Guillaume Simons <guillaume.simons@gmail.com>
2020-04-27 17:32:26 +0200

    clear prints

--- WebApp/classes/model.py
+++ WebApp/classes/model.py
@@ -228,9 +228,9 @@ class Model:
         return model
 
     def generate_projection(actuals, param, R0s):
-        print(actuals)
-        print(param)
-        print(R0s)
+        #print(actuals)
+        #print(param)
+        #print(R0s)
         
         start_time = time.time()
         
@@ -279,44 +279,9 @@ class Model:
         
         end_time = time.time()
         
-        print(model)
+        #print(model)
         
         print("--- %s seconds ---" % (end_time - start_time))
-  
-    #TO DO
-    def set_date(self):
-      self.data["date"] = []
-  
-    def calc_growth_factor(self, measures):
-      self.data["growth_factor"] = []
-  
-  
-    def calc_confirmed_cases(self, parameters):
-      self.data["confirmed_cases"] = []
-  
-    def calc_cumul_confirmed_cases(self, measures):
-      self.data["cumul_confirmed_cases"] = []
-  
-    def calc_real_infected(self, parameters):
-      self.data["cumul_confirmed_cases"] = []
-  
-    def calc_transmission_rate(self):
-      self.data["transmission_rate"] = []
-  
-    def calc_contagious(self, parameters):
-      self.data["contagious"] = []
-  
-    def calc_contagious_pc(self, parameters):
-      self.data["contagious_pc"] = []
-  
-    def calc_cumul_real_infected(self, parameters):
-      self.data["cumul_real_infected"] = []
-  
-    def calc_pop_infected_recovered_pc(self, parameters):
-      self.data["pop_infected_recovered_pc"] = []
-  
-    def calc_pop_susceptible_pc(self):
-      self.data["pop_susceptible_pc"] = []
 
 
 def main():
